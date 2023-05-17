74952b378d22c751c487c86e49ee4b9576b966ea
JanGordon <timmyjan46@gmail.com>
2023-05-17 22:20:46 +0100

    add isServer function and promise hydration

--- js/index.ts
+++ js/index.ts
@@ -47,15 +47,32 @@ export function hydrate(component: HydrateableFunction, element: HTMLElement, ct
     // we need to do difffing to ensure there is no perfoance problems
 }
 
-export function hydr(serverHTML: string, clientHTML: string, name: string, ctx: Ctx) {
+export function hydr(serverHTML: string, clientHTML: string | Promise<string>, name: string, ctx: Ctx) {
     // ctx.nodeId = 0
     if (ctx.isServer) {
         return createHTMLNode(serverHTML, name, ctx, true)
     } else {
-        var d = createHTMLNode(clientHTML, name, ctx, true)
-        return d
+        if (typeof clientHTML == "string") {
+            return createHTMLNode(clientHTML, name, ctx, true)
+        } else {
+            // would be better if we could somehow share state between server and client
+            // but sadly, we have to rerender the server component, and then hydrate once the promise is resolved
+            var d = createHTMLNode(serverHTML, name, ctx, true)
+            var tmpEl = document.createElement("div")
+            tmpEl.innerHTML = d
+            clientHTML
+                .then((val: string)=>{
+                    document.querySelector("[homegrown-hydrate='"+tmpEl.children[0].getAttribute("homegrown-hydrate")+"']")!.innerHTML = val
+                        
+                })
+            return d
+        }
+        
+        
     }
 }
+
+
 
 export function createHTMLNode(s: string, name: string, ctx: Ctx, needsHydration: boolean) {
     ctx.nodeId++
@@ -79,3 +96,5 @@ export function createHTMLNode(s: string, name: string, ctx: Ctx, needsHydration
 export function mkProps(p: [string, Prop][])  : Map<string, Prop> {
     return new Map<string, Prop>(p)
 }
+
+export var isBrowser=new Function("try {return this===window;}catch(e){ return false;}");
